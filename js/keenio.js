c7cce0a0805698d6fa660b3d45b3701fed4ae327
Ari Kamlani <akamlani@gmail.com>
2014-09-22 18:51:07 -0700

    update for control panel processing

--- js/keenio.js
+++ js/keenio.js
@@ -1,4 +1,6 @@
 console.log("Entry Point for Shepherd Voltage Measurement");
+var config = [];
+var client = createClient();
 
 function createClient() {
 		var write_key =  "32bed1b95f93f32f7f5f5802e8e" +
@@ -22,60 +24,63 @@ function createClient() {
 		return new Keen({projectId: "5419e2397d8cb927fb3bf8a5", writeKey: write_key, readKey: read_key});
 }
 
+function jsonToCsv(objArray) {
+	var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+	var str = '';
+	for (var i = 0; i < array.length; i++) {
+	    var line = '';
+	    for (var index in array[i]) {
+	        if (line != '') line += ','
 
-function configCountQuery() {
-		var query_data = {eventCollection: "voltageMeasurement", groupBy: "property"/*, timeframe: "today"*/};
-		return new Keen.Query("count", query_data);
+	        line += array[i][index];
+	    } 
+	    str += line + '\r\n';
+	}
+	return str;
 }
 
-function configMetricQuery(query_type) {
-/*	
-		///timeframe: "today"
-		var metric_query_data = {eventCollection: "voltageMeasurement", groupBy: "property", 
-								 targetProperty: "voltage.cell1"};
-		var metric_percentile_data = {eventCollection: "voltageMeasurement", groupBy: "property", 
-								 targetProperty: "voltage.cell1", percentile: "95.0"/};
+function getSamplesFormat(format_type) {
+	var query_data = {eventCollection: "voltageMeasurement", groupBy: "property"};
+	var extraction = new Keen.Query('extraction', query_data);
+	client.run(extraction, function(response) {
+		var tabular_data = [];
+		tabular_data.push({"id": "BOARD ID", "timestamp": "TIMESTAMP", 
+						   "cell1": "VOLTAGE (Cell1)", "cell2": "VOLTAGE (Cell2)"});
 
-		var metrics_max = new Keen.Query("maximum", metric_query_data);
-		var metrics_min = new Keen.Query("minimum", metric_query_data);
-		var metrics_avg = new Keen.Query("average", metric_query_data);
-		var metrics_median = new Keen.Query("median", metric_query_data);
-		var metrics_percentile = new Keen.Query("percentile", metric_percentile_data);
-
-		//summary statistics
-		//@todo: Fix for Multi-Analysis Usage API
-		//@todo: Add GroupBy (Agent, Specific ID) = "group_by" -> "board.id (macaddress)"
-		var labels = ["Number of Records: ", "Minimum Voltage: ", "Maximum Voltage: ", 
-				  	  "Average Voltage: ", "Median Voltage: ", "Percentile Voltage (95%): "];
-
-
-		var mashup_query = [count, metrics_min, metrics_max, metrics_avg, metrics_median, metrics_percentile];
-		var mashup = client.run(mashup_query, function(response) {
-			for(var idx in response) {
-				var metric = response[idx].result[0].result;
-				metric = metric.toFixed(3);
-				var metric_label = "<b>" + labels[idx] + "</b>";
-				$("#analytics-metrics").append(metric_label + metric + "<br>");
-			}
-		});
+		var data_samples = response.result;
+		for(var idx in data_samples) {
+			var entry = data_samples[idx];
+			var row = {"id": config["Board Name"], "timestamp": Date(entry.keen.timestamp), 
+					   "cell1": entry.voltage.cell1, "cell2": entry.voltage.cell2};
+			tabular_data.push(row);
+		}
+		//option: write to csv format
+		var jsonObject = JSON.stringify(tabular_data);
+		var csv_data = jsonToCsv(jsonObject);
+		
+/*		
+var a = document.createElement('a');
+a.href     = 'data:attachment/csv,' + csv_data;
+a.target   ='_blank';
+a.download = 'myFile.csv,' + encodeURIComponent(csv_data); ;
+a.innerHTML = "Click me to download the file.";
+document.body.appendChild(a);
 */
+
+		//option: keep as json format
+	});
 }
 
-function getSamples() {
-		var query_data = {eventCollection: "voltageMeasurement", groupBy: "property"};
-		var extraction = new Keen.Query('extraction', query_data);
-		return extraction;
-}
 
 function displaySamples(data_samples, max_samples) {
 	for(var idx in data_samples) {
 		var entry = data_samples[idx];
-		var row = {"id": "UHF_RFID", "timestamp": entry.keen.timestamp, 
+		var row = {"id": config["Board Name"], "timestamp": entry.keen.timestamp, 
 				   "cell1": entry.voltage.cell1, "cell2": entry.voltage.cell2};
 
 		//Create Table to Push into Google Docs
 		tabular_data.push(row);
-		
+/*		
 		if(idx < max_samples) {
 			var cell1_entry = "<tr>" +
 							  "<td>" + idx + "</td>" +
@@ -92,23 +97,83 @@ function displaySamples(data_samples, max_samples) {
 			$("#battery-samples-cell1").append(cell1_entry);
 			$("#battery-samples-cell2").append(cell2_entry);
 		}
+*/
 	}
+	//Create CSV
 
+
+}
+
+function metricsForCells(target_property_val) {
+	//add timeframe: "today"
+	//add interval:	
+	var count_query_data = {eventCollection: "voltageMeasurement", groupBy: "property"};
+
+	var metric_query_data = {eventCollection: "voltageMeasurement", groupBy: "property", 
+							 targetProperty: target_property_val};
+	var metric_percentile_data = {eventCollection: "voltageMeasurement", groupBy: "property", 
+							 	  targetProperty: target_property_val, percentile: "95.0"};
+
+	var count_samples = new Keen.Query("count", count_query_data);	
+	var metrics_max = new Keen.Query("maximum", metric_query_data);
+	var metrics_min = new Keen.Query("minimum", metric_query_data);
+	var metrics_avg = new Keen.Query("average", metric_query_data);
+	var metrics_median = new Keen.Query("median", metric_query_data);
+	var metrics_percentile = new Keen.Query("percentile", metric_percentile_data);
+
+
+	var metric_label = "";
+	var labels = ["Number of Records: ", "Minimum Voltage: ", "Maximum Voltage: ", 
+			  	  "Average Voltage: ", "Median Voltage: ", "Percentile Voltage (95%): "];
+	var mashup_query = [count_samples, metrics_min, metrics_max, metrics_avg, metrics_median, metrics_percentile];
+	var mashup = client.run(mashup_query, function(response) {
+		for(var idx in response) {
+			var metric = response[idx].result[0].result;
+			metric = metric.toFixed(3);
+			metric_label += "<b>" + labels[idx] + "</b><br>" + metric + "<br>";
+		}
+
+		if(target_property_val == "voltage.cell1") {	
+			$("#analytics-metrics-cell1").html( (metric_label) );
+		}
+		if(target_property_val == "voltage.cell2") {
+			$("#analytics-metrics-cell2").html( (metric_label) );
+		}
+	});
+}
+
+
+function monitorForCells() {
+	var json_data = getSamplesFormat("csv");
+	
+
+}
+
+function setConfiguration() {
+	config =  {
+		"Board Name": document.getElementById('ctl-board-name').value,
+		"Sample Period": document.getElementById('ctl-samplerate').value,
+		"Monitor Cell1": $("#v_cell1").prop('checked'),
+		"Monitor Cell2": $("#v_cell2").prop('checked'),
+		"Metrics Cell1": $("#m_cell1").prop('checked'),
+		"Metrics Cell2": $("#m_cell2").prop('checked'),
+	};
+	//Metrics
+	if(config["Metrics Cell1"]) { metricsForCells("voltage.cell1"); }
+	if(config["Metrics Cell2"]) { metricsForCells("voltage.cell2"); }
+	//Voltage Monitoring
+	if(config["Monitor Cell1"]) { monitorForCells("voltage.cell1"); }
+	//if(config["Monitor Cell2"]) {}
+	//CSV Format
+	//getSamplesFormat("json");
 }
 
 
 
-var client = createClient();
-var count  = configCountQuery();
 
-var mashup = client.run(count, function(response) {
-	var data = response.result[0].result;
-	var metric_label = "<b>" + "# Samples: " + "</b>";
-	$("#analytics-metrics-cell1").append(metric_label + data + "<br>");
-	$("#analytics-metrics-cell2").append(metric_label + data + "<br>");
 
-});
 
+/*
 var tabular_data = [];
 var samples = getSamples(client, tabular_data);
 var data = client.run(samples, function(response){
@@ -120,4 +185,4 @@ var data = client.run(samples, function(response){
 	displaySamples(keen_json, 5);
 	//Push into Google Docs - Google Spreadsheets "Add-Ons"
 });
-
+*/
